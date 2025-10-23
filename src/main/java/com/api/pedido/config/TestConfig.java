8440cf1cf4752edac63ded1e0f43d15833899316
Mauricio Zazeri Fonseca <maurz.fonseca@gmail.com>
2025-10-23 13:32:59 -0300

    Create TestConfig.java
    Adicionado uma classe de configuração no profile test para que possamos inserir automaticamente alguns dados em nossa tabela assim que o programa roda..

---
+++ src/main/java/com/api/pedido/config/TestConfig.java
@@ -0,0 +1,27 @@
+package com.api.pedido.config;
+
+
+import com.api.pedido.model.User;
+import com.api.pedido.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        User u1 = new User(null, "Mauricio", "maur@test.com", "19999662703", "123456");
+        User u2 = new User(null, "Ana", "ana@test.com", "19999662905", "12345678");
+
+        userRepository.saveAll(Arrays.asList(u1, u2));
+    }
+}

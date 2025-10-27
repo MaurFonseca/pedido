45399e74dba7557b3aff5eb2ceecbfdae36dc5b0
Mauricio Zazeri Fonseca <maurz.fonseca@gmail.com>
2025-10-27 11:29:07 -0300

    Create ProductService.java

---
+++ src/main/java/com/api/pedido/service/ProductService.java
@@ -0,0 +1,26 @@
+package com.api.pedido.service;
+
+
+import com.api.pedido.model.Product;
+import com.api.pedido.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+
+    public List<Product> findAll(){
+        return productRepository.findAll();
+    }
+
+    public Product findById(Long id){
+        return productRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("Produto não encontrado"));
+    }
+}
